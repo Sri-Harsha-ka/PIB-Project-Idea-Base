ba0de1d0aca55b4e8e774e32972813bc222fbc25
Harsha <sriharshakaredla@gmail.com>
2026-01-08 09:51:58 +0530

    Added a Loading Animation

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { motion } from "framer-motion"
 
 const Loading = () => {
-  return (
-    <div>
-      Loading.... 
-      <p>Please wait , the backend is in render and needs a cold start (i ain't rich bruu...)</p>
-    </div>
-  )
+	return (
+		<div>
+
+			<motion.div initial={{rotate: 0}} animate={{rotate: 360}} transition={{duration: 1 , ease: 'linear', repeat: Infinity}} className='bg-white w-32 h-32 rounded-full relative flex justify-center items-center overflow-hidden'>
+				<div className='bg-[#1E201E] w-28 h-28 rounded-full absolute z-10'>
+				</div>
+				<div className='bg-blue-500 w-10 h-10 bottom-0 absolute'></div>
+			</motion.div>
+
+		</div>
+	)
 }
 
 export default Loading
